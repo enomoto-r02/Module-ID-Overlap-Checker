aaedfc5bfe835b22dc1d97aa75fe40ca69f5c4ad
榎本 瞬 <enomoto.r02@outlook.jp>
2024-08-24 09:33:36 +0900

    修正

--- Module ID Overlap Checker/Result.cs
+++ Module ID Overlap Checker/Result.cs
@@ -24,17 +24,12 @@ namespace Module_ID_Overlap_Checker
 
             foreach (var costbl_cos_id in costbl_cos_ids)
             {
-                // xxxitm_tblからcos.xxxxx.id=cosidの添字を取得
                 var cosId_id = this.Mod.ModDB.GetItemTblByValueRegex(@"cos\.\d+\.id", costbl_cos_id.Value);
                 if (cosId_id.Count == 0)
                 {
                     continue;
                 }
 
-                //var cosId_id2_int = int.Parse(cosId_cos[0].Value);
-                //var cosId_id2 = this.Mod.ModDB.CosIdByValue(@"cos\.\d\.id", (cosId_id2_int-1).ToString());
-
-                // xxxitm_tblからcos.n.id.length=xxxxxを取得
                 var cosId_length = this.Mod.ModDB.GetItemTblByRegex(@"cos." + cosId_id[0].Parameter[1] + ".item.length");
                 if (cosId_length.Count == 0)
                 {
@@ -44,7 +39,6 @@ namespace Module_ID_Overlap_Checker
                 List<Item> cos_parts = new List<Item>();
                 for (int i=0; i<cosId_length.Count; i++)
                 {
-                    // xxxitm_tblからcos.n.id.item=xxxxxを取得
                     cos_parts.Add(this.Mod.ModDB.GetItemTblByKey(@"cos." + cosId_id[0].Parameter[1] + ".item."+i));
                 }
                 if (cos_parts.Count == 0)
@@ -52,22 +46,23 @@ namespace Module_ID_Overlap_Checker
                     continue;
                 }
 
-                // cos.n.id.item数ループ
                 for (int i = 0; i < int.Parse(cosId_length[0].Value); i++)
                 {
                     if (cosId_length.Count == 0) continue;
 
                     try
                     {
-                        var itemno = this.Mod.ModDB.GetItemTblByKey("item." + index + ".no");
+                        var item_index = this.Mod.ModDB.GetItemTblByValueRegex(@"item\.\d+\.no", cos_parts[i].Value);
+                        if (item_index.Count == 0) continue;
+                        var itemno = this.Mod.ModDB.GetItemTblByKey("item." + item_index[0].Parameter[1] + ".no");
                         if (itemno == null) continue;
-                        var subid = this.Mod.ModDB.GetItemTblByKey("item." + i + ".sub_id");
+                        var subid = this.Mod.ModDB.GetItemTblByKey("item." + item_index[0].Parameter[1] + ".sub_id");
                         if (subid == null) continue;
-                        var name = this.Mod.ModDB.GetItemTblByKey("item." + index + ".name");
+                        var name = this.Mod.ModDB.GetItemTblByKey("item." + item_index[0].Parameter[1] + ".name");
                         if (name == null) continue;
                         var module_id = this.Mod.ModDB.GmModuleItemTblByKey(@"module." + cosId_id[0].Parameter[1] + ".id");
                         if (module_id == null) continue;
-                        var lang_val = this.Mod.GetArrayStr(config, subid.Value, module_id.Value, "");
+                        var lang_val = this.Mod.GetArrayStr(config, subid.Value, cosId_id[0].Value, "");
 
                         sb.Append(string.Join("\t",
                             this.Mod.Name,
